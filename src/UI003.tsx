936847798adbe8e67de61b603b7aa2ce8f63f3f1
Justin9606 <tonybek.96@gmail.com>
2023-03-20 17:55:30 +0900

    created UI003

---
+++ src/UI003.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+  id?: string;
+  imageUrl?: string | undefined;
+  borderRadius?: number;
+};
+
+const UI003: React.FC<Props> = ({ imageUrl, borderRadius }) => {
+  return <StyledImage borderRadius={borderRadius} src={imageUrl} />;
+};
+
+export default UI003;
+
+const StyledImage = styled.img<{ borderRadius?: number }>`
+  height: 164px;
+  border-radius: ${(props) => props.borderRadius || 10}px;
+  overflow: hidden;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-top: 24px;
+`;
